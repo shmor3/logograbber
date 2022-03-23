cf175b832e448fb291b57729cafd690ff0b375b1
Robert <44666181+shmor3@users.noreply.github.com>
2022-03-23 07:12:37 -0600

    Pathing Updates 😵‍💫

--- util/getImg.py
+++ util/getImg.py
@@ -2,25 +2,27 @@ import requests
 import shutil
 from bs4 import BeautifulSoup
 
-
 url = 'http://fotw.ethnia.org/flags/us-co-dv.html'
-base_url = 'http://fotw.ethnia.org'
-output_dir = '/data/todo'
-page = requests.get(url, stream = True, headers={"User-agent": "Mozilla/5.0"})
-soup = BeautifulSoup(page.content, 'html.parser')
-souf = soup.find('img', alt='[City Seal]')
-img_url = base_url + souf.attrs['src'].replace('..', '')
-print(img_url)
 
-filename = img_url.split("/")[-1]
-r = requests.get(img_url, stream = True)
-if r.status_code == 200:
-    # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
-    r.raw.decode_content = True
-    
-    # Open a local file with wb ( write binary ) permission.
-    with open(filename,'wb') as f:
-        shutil.copyfileobj(r.raw, f)
-        moV = shutil.move(str(filename), './data/todo')
-else:
-    pass
+def getImg(url):
+    base_url = 'http://fotw.ethnia.org'
+    output_dir = '/data/tmp/todo'
+    page = requests.get(url, stream = True, headers={"User-agent": "Mozilla/5.0"})
+    soup = BeautifulSoup(page.content, 'html.parser')
+    souf = soup.find('img', alt='[City Seal]')
+    img_url = base_url + souf.attrs['src'].replace('..', '')
+    print(img_url)
+
+    filename = img_url.split("/")[-1]
+    r = requests.get(img_url, stream = True)
+    if r.status_code == 200:
+        # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+        r.raw.decode_content = True
+        
+        # Open a local file with wb ( write binary ) permission.
+        with open(filename,'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+            moV = shutil.move(str(filename), output_dir)
+    else:
+        pass
+getImg()
--- util/resize.py
+++ util/resize.py
@@ -2,17 +2,18 @@ import os
 import glob
 from PIL import Image
 
-dst_dir = 'data/done'
-os.makedirs(dst_dir, exist_ok=True)
+def resize():
+    dst_dir = 'data/tmp/done'
+    os.makedirs(dst_dir, exist_ok=True)
 
-files = glob.glob('./data/todo/*')
-for f in files:
-    try:
-        img = Image.open(f)
-        print(img.size)
-        img_resize = img.resize((150, 150))
-        root, ext = os.path.splitext(f)
-        basename = os.path.basename(root)
-        img_resize.save(os.path.join(dst_dir, basename + '.png'))
-    except OSError as e:
-        pass
+    files = glob.glob('./data/tmp/todo/*')
+    for f in files:
+        try:
+            img = Image.open(f)
+            print(img.size)
+            img_resize = img.resize((150, 150))
+            root, ext = os.path.splitext(f)
+            basename = os.path.basename(root)
+            img_resize.save(os.path.join(dst_dir, basename + '.png'))
+        except OSError as e:
+            pass
