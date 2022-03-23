057b9ddc18d7fdf5388eeb34e10c7d6a565746ab
Robert <44666181+shmor3@users.noreply.github.com>
2022-03-23 08:07:13 -0600

    added functions, updated file system

--- utils/getImg.py
+++ utils/getImg.py
@@ -1,28 +1,26 @@
+import os
 import requests
 import shutil
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 
-url = 'http://fotw.ethnia.org/flags/us-co-dv.html'
-
-def getImg(url):
-    base_url = 'http://fotw.ethnia.org'
-    output_dir = '/data/tmp/todo'
+def getImg():
+    load_dotenv()
+    url = os.environ['url']
+    base_url = os.environ['base_url']
+    output_dir = '../data/tmp/todo'
     page = requests.get(url, stream = True, headers={"User-agent": "Mozilla/5.0"})
     soup = BeautifulSoup(page.content, 'html.parser')
     souf = soup.find('img', alt='[City Seal]')
     img_url = base_url + souf.attrs['src'].replace('..', '')
-    print(img_url)
-
     filename = img_url.split("/")[-1]
+
     r = requests.get(img_url, stream = True)
     if r.status_code == 200:
-        # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
         r.raw.decode_content = True
-        
-        # Open a local file with wb ( write binary ) permission.
         with open(filename,'wb') as f:
             shutil.copyfileobj(r.raw, f)
             moV = shutil.move(str(filename), output_dir)
     else:
-        pass
+        print('fail')
 getImg()
--- utils/resize.py
+++ utils/resize.py
@@ -1,19 +1,21 @@
 import os
 import glob
 from PIL import Image
+from dotenv import load_dotenv
 
 def resize():
-    dst_dir = 'data/tmp/done'
+    load_dotenv()
+    dst_dir = './data/tmp/done'
     os.makedirs(dst_dir, exist_ok=True)
-
     files = glob.glob('./data/tmp/todo/*')
     for f in files:
         try:
             img = Image.open(f)
             print(img.size)
-            img_resize = img.resize((150, 150))
+            img_resize = img.resize((os.environ['img_size']))
             root, ext = os.path.splitext(f)
             basename = os.path.basename(root)
-            img_resize.save(os.path.join(dst_dir, basename + '.png'))
+            img_resize.save(os.path.join(dst_dir, basename + os.environ['saveImgAs']))
         except OSError as e:
             pass
+resize()
