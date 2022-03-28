db76047c1454c939ebfa14ce5945c760e091fafc
Robert <44666181+shmor3@users.noreply.github.com>
2022-03-28 15:06:25 -0600

    Global Proxy

--- run.py
+++ run.py
@@ -5,25 +5,27 @@ from bs4 import BeautifulSoup
 from PIL import Image
 
 load_dotenv()
+
+proxiesList = tuple(open(str(os.environ['proxyList']), 'r'))
+for proxy in proxiesList:
+    try:
+        proxyfrrm = 'http://' + str(proxy)
+        print(u'\n\u001b[33mChecking Proxy:\n', proxyfrrm)
+        proxPage = requests.get(os.environ['proxyTestUrl'], headers={"User-agent": "Mozilla/5.0"}, proxies={"http": proxyfrrm, "https": proxyfrrm})
+        time.sleep(1)
+        print(u'\n\u001b[32mStatus',proxPage.status_code,'OK','PASS:\n', '\u001b[36;1mProxy IP', proxPage.text)
+    except OSError as e:
+        print(u'\n\u001b[31mStatus',e,'FAIL:\n', proxyfrrm, '\u001b[0m\n')
+        with open(os.environ['proxyList'], "r+") as f:
+            d = f.readlines()
+            f.seek(0)
+            for i in d:
+                if i != str(proxy):
+                    f.write(i)
+                    f.truncate()
 class sealsnatcher():
     def search():
-        proxiesList = tuple(open(str(os.environ['proxyList']), 'r'))
-        for proxy in proxiesList:
-            try:
-                proxyfrrm = 'http://' + str(proxy)
-                print(u'\n\u001b[33mChecking Proxy:\n', proxyfrrm)
-                proxPage = requests.get(os.environ['proxyTestUrl'], headers={"User-agent": "Mozilla/5.0"})
-                time.sleep(1)
-                print(u'\n\u001b[32mStatus',proxPage.status_code,'OK','PASS:\n', '\u001b[36;1mProxy IP', proxPage.text)
-            except OSError as e:
-                print(u'\n\u001b[31mStatus',e,'FAIL:\n', proxyfrrm, '\u001b[0m\n')
-                with open(os.environ['proxyList'], "r+") as f:
-                    d = f.readlines()
-                    f.seek(0)
-                    for i in d:
-                        if i != str(proxy):
-                            f.write(i)
-                            f.truncate()
+
         keyWordList = tuple(open(str(os.environ['keywordList']), 'r'))
         for keyWord in keyWordList:
             try:
@@ -31,7 +33,7 @@ class sealsnatcher():
                 print(u'\n', '\u001b[32m@' + 'google.com|' + str(query), '\n\u001b[36;1mSearching:', '\u001b[33m')
                 search_result = list(search(query, tld="com", num=1, stop=1, pause=10))
                 time.sleep(1)
-                page = requests.get(search_result[0], headers={"User-agent": "Mozilla/5.0"})
+                page = requests.get(search_result[0], headers={"User-agent": "Mozilla/5.0"}, proxies={"http": proxyfrrm, "https": proxyfrrm})
                 search.u = str(search_result[0]).replace('https://www.crwflags.com/fotw/flags/', '')
                 print(u'\n\u001b[32mStatus', page.status_code, 'OK', 'PASS')
                 urlList = open(os.environ['urlListDir'], "a")
@@ -39,6 +41,7 @@ class sealsnatcher():
                 urlList.close()
             except OSError as e:
                 print('SEARCH ERROR', e)
+    search()
     def dlImg():
         urlList = tuple(open(str(os.environ['urlListDir']), 'r'))
         for url in urlList:
@@ -51,15 +54,14 @@ class sealsnatcher():
                 localRoute = os.environ['localRoute']
                 urlr = localUrl.lower() + localRoute + url.replace('.html', ').gif')
                 urlp = baseUrl.lower() + urlRoute + url
-                page = requests.get(urlp)
+                page = requests.get(urlp, {"User-agent": "Mozilla/5.0"}, proxies={"http": proxyfrrm, "https": proxyfrrm})
                 time.sleep(1)
                 soup = BeautifulSoup(page.content, 'lxml')
                 soupd = soup.find('img', alt='[City Seal]')
                 soupf = soupd.attrs['src'].replace('../', '')
                 imgUrl = urlr.replace(url, '')
                 filename = imgUrl.split("/")[-1]
-                time.sleep(1)
-                r = requests.get(imgUrl, stream = True)
+                r = requests.get(imgUrl, stream = True, headers={"User-agent": "Mozilla/5.0"}, proxies={"http": proxyfrrm, "https": proxyfrrm})
                 time.sleep(1)
                 if r.status_code == 200:
                     r.raw.decode_content = True
