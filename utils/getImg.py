d9afb43f9c0795eef8da8e6a006ddedfe723e675
Robert <44666181+shmor3@users.noreply.github.com>
2022-03-27 19:53:24 -0600

    Bug Fixes

--- utils/getImg.py
+++ utils/getImg.py
@@ -1,37 +1,38 @@
-import os, requests, shutil, time
+import os, requests, shutil, time, lxml
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
+load_dotenv()
+urlListPath = str(os.environ['urlListDir'])
+urlList = tuple(open(urlListPath, 'r'))
 class getImg():
     def opera():
-        load_dotenv()
-        filepath = str(os.environ['urlListDir'])
-        with open(filepath) as fp:
-            line = fp.readline()
-            cnt = 1
-            while line:
-                line = fp.readline()
-                cnt += 1
+        for url in urlList:
+            try:
+                urlfrrm = str(url)
                 baseUrl = str(os.environ['baseUrl'])
-                url = str(os.environ['baseUrl']) + str(os.environ['urlEnding']) + str('{}'.format(line.strip()))
+                url = str(os.environ['baseUrl']) + str(os.environ['urlEnding']) + urlfrrm
                 output_dir = os.environ['dlDir']     
                 time.sleep(1)
-                page = requests.get(url, stream = True, headers={"User-agent": "Mozilla/5.0"})
+                page = requests.get(url, stream = True)
                 soup = BeautifulSoup(page.content, 'html.parser')
-                souf = soup.find(os.environ['img'], alt=os.environ['alt'])
-                img_url = baseUrl + souf.attrs[os.environ['src']].replace('..', '')
-                filename = img_url.split("/")[-1]
+                souf = soup.find(os.environ['type'], alt=os.environ['altTag'])
+                imgUrl = baseUrl + souf.attrs['src'].replace('..', '')
+                filename = imgUrl.split("/")[-1]
                 print('Image Found')
                 time.sleep(1)
-                r = requests.get(img_url, stream = True)
+                r = requests.get(imgUrl, stream = True)
                 if r.status_code == 200:
                     r.raw.decode_content = True
                     with open(filename,'wb') as f:
                         shutil.copyfileobj(r.raw, f)
                         moV = shutil.move(str(filename), output_dir)
-                        print(u'\u001b[32mDownloaded Successfully\n')
-                else:
-                    print(u'\u001b[31mDownload Failed\n')
+                        print(u'\n\u001b[32mDownloaded Successfully\n')
                     pass
+                else:
+                    print(u'\n\u001b[31mDownload Failed\n')
+                    pass
+            except OSError as e:
+                print('Critical Error', e)
     opera()
 getImg()
